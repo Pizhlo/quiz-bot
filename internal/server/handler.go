4127067c6e5e26b30b6df8064b28a90e8904aa00
Maria <zaush@bk.ru>
2025-01-18 15:44:24 +0300

    added main menu

--- internal/server/handler.go
+++ internal/server/handler.go
@@ -3,13 +3,14 @@ package server
 import (
 	"quiz-mod/internal/command"
 	"quiz-mod/internal/message"
+	"quiz-mod/internal/view"
 
 	"gopkg.in/telebot.v3"
 )
 
 func (s *Server) commands() {
 	s.bot.Handle(command.Start, func(ctx telebot.Context) error {
-		return ctx.EditOrSend(message.StartMessage)
+		return ctx.EditOrSend(message.StartMessage, view.MainMenu())
 	})
 
 	s.bot.Handle(command.Help, func(ctx telebot.Context) error {
